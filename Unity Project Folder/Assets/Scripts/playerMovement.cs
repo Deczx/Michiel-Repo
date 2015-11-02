3e26ccba9a8636a2076401a28ccaff57bcddf74b
Michiel Boekhoff <michielalexanderb@gmail.com>
2015-11-02 23:05:52 +0100

    Fixed movement

--- Unity Project Folder/Assets/Scripts/playerMovement.cs
+++ Unity Project Folder/Assets/Scripts/playerMovement.cs
@@ -5,20 +5,18 @@ public class playerMovement : MonoBehaviour
 {
 
     public float horizontalMovementSpeed;
-    public float horizontalMaxSpeed;
     public float verticalMovementSpeed;
-    public float verticalMaxSpeed;
 
     private Rigidbody2D rb;
 
     // Use this for initialization
     void Start()
     {
-        horizontalMovementSpeed = 4f;
-        horizontalMaxSpeed = 4f;
+        //horizontalMovementSpeed = 4f;
+        //horizontalMaxSpeed = 4f;
 
-        verticalMovementSpeed = 4f;
-        verticalMaxSpeed = 4f;
+        //verticalMovementSpeed = 4f;
+        //verticalMaxSpeed = 4f;
 
         rb = GetComponent<Rigidbody2D>();
     }
@@ -31,7 +29,7 @@ public class playerMovement : MonoBehaviour
         float v = Input.GetAxis("Vertical");
 
         // Set movement to according key pressed
-        if (Input.GetKey(KeyCode.D)) rb.velocity += (Vector2.right * h) * horizontalMovementSpeed;
+        /*if (Input.GetKey(KeyCode.D)) rb.velocity += (Vector2.right * h) * horizontalMovementSpeed;
         if (Input.GetKey(KeyCode.A)) rb.velocity -= (Vector2.left * h) * horizontalMovementSpeed;
         if (Input.GetKey(KeyCode.W)) rb.velocity += (Vector2.up * v) * verticalMovementSpeed;
         if (Input.GetKey(KeyCode.S)) rb.velocity -= (Vector2.down * v) * verticalMovementSpeed;
@@ -42,7 +40,9 @@ public class playerMovement : MonoBehaviour
         if (rb.velocity.x <= -4f) rb.velocity = new Vector2(-horizontalMaxSpeed, rb.velocity.y);
         if (rb.velocity.y >= 4f) rb.velocity = new Vector2(rb.velocity.x, verticalMaxSpeed);
         if (rb.velocity.y <= -4f) rb.velocity = new Vector2(rb.velocity.x, -verticalMaxSpeed);
+         */
 
+        rb.velocity = new Vector2(h * horizontalMovementSpeed, v * verticalMovementSpeed);
     }
 
     // handle collisions
