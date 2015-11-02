2a88e963bf658cc7a199cfd444b52c4807a15fba
Jeff Kloos <jeffrey.kloos@gmail.com>
2015-11-03 00:58:46 +0100

    My work stuffs

---
+++ Unity Project Folder/Assets/BulletManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletManager : MonoBehaviour {
+
+    public static int ammo;
+    public static int ammoBlue;
+    public static int ammoGreen;
+    public static int ammoRed;
+	
+    // Use this for initialization
+	void Start () {
+        ammo = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
