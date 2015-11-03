032645997469771e9f521507f63fec1b518db815
Michiel Boekhoff <michielalexanderb@gmail.com>
2015-11-03 23:26:19 +0100

    Made initial version of enemy manager.

---
+++ Unity Project Folder/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+
+    bool is_started = false;
+    bool first_wave_started = false;
+
+    int level = 0;
+    public int currency = 2500;
+    public int level_modifier = 2;
+
+    int count_enemies = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!first_wave_started)
+        {
+            first_wave_started = true;
+            StartWave();
+        }
+
+        if (count_enemies == 0 && is_started == true)
+        {
+            //MOVE TO STORE!
+        }
+	}
+
+    void StartWave()
+    {
+        if (is_started == false)
+        {
+            is_started = true;
+            int num_spawns = GameObject.FindGameObjectsWithTag("Spawn").Length;
+            BroadcastMessage("Spawn", currency / num_spawns); //int division
+        }
+    }
+
+    void Spawned()
+    {
+        ++count_enemies;
+    }
+
+    void Killed()
+    {
+        --count_enemies;
+    }
+}
